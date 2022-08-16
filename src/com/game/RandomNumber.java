16cb1a2ee2aa282c99f0c7cbd2743b80e9d0caaa
Kiran AH <kiran.ah@valtech.com>
2022-08-16 13:00:25 +0530

    First commit1

---
+++ src/com/game/RandomNumber.java
@@ -0,0 +1,37 @@
+package com.game;
+
+
+import java.util.*;
+
+public class RandomNumber {
+	public static void main(String[] args) {
+	
+		int randomNumber=(int)(Math.random()*100);
+		try (Scanner scan = new Scanner(System.in)) {
+			int gusserNumber=0;
+			
+			
+			do {
+				System.out.print("Enter your number b/w 1-100 :");
+				 gusserNumber= scan.nextInt();
+				
+				if(gusserNumber == randomNumber) {
+					System.out.println("Wohoo..! you are Correct");
+					break;
+				}
+				
+				else if(gusserNumber < randomNumber) {
+					System.out.println("Bhari kammi athale");
+					
+					
+				}
+				else {
+					System.out.println("Bhari myal hogbitti");
+				}
+			}while(gusserNumber>=0);
+		}
+		System.out.println("My number is :"+randomNumber);
+	}
+}
+
+
